f09b2f7b4e47c1bc4bbf10cc9dd55d9f29159dde
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-04-01 22:10:17 +0400

    Update d2.java

--- d2.java
+++ d2.java
@@ -1,20 +1,18 @@
-public class d2{
-  public static void main (String[] arg) throws Exception{
-    
-    Student student1=new Student();
-    Student student2=new Student();
+public class d2 {
+  public static void main(String[] arg) throws Exception {
+
+    Student student1 = new Student();
+    Student student2 = new Student();
     student1.SetName("Vasif");
-    student1.SetEmail("vasif@gmail.com"); 
+    student1.SetEmail("vasif@gmail.com");
     student1.SetAge(9);
     student2.SetName("Tahir");
     student2.SetEmail("tahir@gmail.com");
     student2.SetAge(20);
-    
- 
-      student1.print();
-    
-      student2.print();
-    
-}
-} 
 
+    student1.print();
+
+    student2.print();
+
+  }
+}
