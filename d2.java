e7dee3c92081b6f5d952e3b0b5b4e3b7baa96343
Geydarova Zulfiya <152681616+Zulya1012@users.noreply.github.com>
2024-04-01 22:01:52 +0400

    Update d2.java

--- d2.java
+++ d2.java
@@ -3,9 +3,9 @@ public class d2{
     
     Student student1=new Student();
     Student student2=new Student();
-    student1.SetName("Vasiiiiifffff");
+    student1.SetName("Vasif");
     student1.SetEmail("vasif@gmail.com"); 
-    student1.SetAge(15);
+    student1.SetAge(9);
     student2.SetName("Tahir");
     student2.SetEmail("tahir@gmail.com");
     student2.SetAge(20);
